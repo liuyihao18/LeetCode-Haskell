9f39006b23ce3bf75e6bbbda0e914197d3034873
刘怡豪 <liuyihao18@foxmail.com>
2023-08-23 12:16:59 +0800

    update util

--- src/util.hs
+++ src/util.hs
@@ -28,3 +28,45 @@ takeRange l r = take (r - l) . drop l
 
 takeNAtP :: Int -> Int -> [a] -> [a]
 takeNAtP p n = take n . drop p
+
+bsearch :: [Int] -> Int -> Bool
+bsearch [] _ = False
+bsearch list target
+  | target > element = bsearch list2 target
+  | target < element = bsearch list1 target
+  | otherwise = True
+  where
+    middle = length list `div` 2
+    element = list !! middle
+    list1 = take middle list
+    list2 = drop (middle + 1) list
+
+lowerBound :: [Int] -> Int -> Int
+lowerBound list target = _lowerBound list target 0 n
+  where
+    n = length list
+
+_lowerBound :: [Int] -> Int -> Int -> Int -> Int
+_lowerBound [] _ _ _ = -1
+_lowerBound list target left right
+  | left >= right = left
+  | target > element = _lowerBound list target (middle + 1) right
+  | otherwise = _lowerBound list target left middle
+  where
+    middle = (left + right) `div` 2
+    element = list !! middle
+
+upperBound :: [Int] -> Int -> Int
+upperBound list target = _upperBound list target 0 n
+  where
+    n = length list
+
+_upperBound :: [Int] -> Int -> Int -> Int -> Int
+_upperBound [] _ _ _ = -1
+_upperBound list target left right
+  | left >= right = left
+  | target >= element = _upperBound list target (middle + 1) right
+  | otherwise = _upperBound list target left middle
+  where
+    middle = (left + right) `div` 2
+    element = list !! middle
