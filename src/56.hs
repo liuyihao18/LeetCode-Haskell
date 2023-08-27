69aadcfa3cee3e3b0599665162fe22569f79e631
刘怡豪 <liuyihao18@foxmail.com>
2023-08-27 10:16:18 +0800

    add 56

---
+++ src/56.hs
@@ -0,0 +1,40 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Solution where
+
+import Data.List
+import IO
+
+merge :: [[Int]] -> [[Int]]
+merge = _merge . sort
+
+_merge :: [[Int]] -> [[Int]]
+_merge [] = []
+_merge [x] = [x]
+_merge (x : y : z)
+  | last x < head y = x : y : _merge z
+  | otherwise = [head x, last x `max` last y] : _merge z
+
+intervals1 :: [[Int]]
+intervals1 = [[1, 3], [2, 6], [8, 10], [15, 18]]
+
+intervals2 :: [[Int]]
+intervals2 = [[1, 4], [4, 5]]
+
+intervals3 :: [[Int]]
+intervals3 = [[1, 4], [2, 3]]
+
+input :: [[Int]] -> IO ()
+input = input1 "intervals"
+
+output :: [[Int]] -> IO ()
+output = output1
+
+main :: IO ()
+main = do
+  input intervals1
+  output (merge intervals1)
+  input intervals2
+  output (merge intervals2)
+  input intervals3
+  output (merge intervals3)
