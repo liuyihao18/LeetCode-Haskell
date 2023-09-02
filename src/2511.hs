7cd73d8da9f7612a0136657b2d052d2aeaf8de3d
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2023-09-02 14:30:34 +0800

    update 2511

--- src/2511.hs
+++ src/2511.hs
@@ -26,13 +26,14 @@ captureForts1 =
 captureForts2 :: [Int] -> Int
 captureForts2 [] = 0
 captureForts2 (pre : forts)
-  | pre == 0 = captureForts2 forts'
+  | pre == 0 = nextCnt
   | otherwise = case forts' of
       [] -> 0
-      (post : _) -> if pre + post == 0 then cnt `max` captureForts2 forts' else captureForts2 forts'
+      (post : _) -> if pre + post == 0 then cnt `max` nextCnt else nextCnt
   where
     forts' = dropWhile (== 0) forts
     cnt = length $ takeWhile (== 0) forts
+    nextCnt = captureForts2 forts'
 
 forts1 :: [Int]
 forts1 = [1, 0, 0, -1, 0, 0, 0, 0, 1]
